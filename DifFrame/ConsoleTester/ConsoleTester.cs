f3a0e2a92787e6c9408c5cac5b42a8b73649d834
Max R <41270557+Ninjatogo@users.noreply.github.com>
2020-11-22 19:22:47 -0500

    Setup console tester project to test new changes.

--- DifFrame/ConsoleTester/ConsoleTester.cs
+++ DifFrame/ConsoleTester/ConsoleTester.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Difframe;
+using ProcessNode;
 
 namespace ConsoleTester
 {
@@ -7,12 +7,39 @@ namespace ConsoleTester
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please supply file directory");
-            var fileDirectory = "SampleFrames-Mob_Psycho_100";//Console.ReadLine();
-            var engine = new ProcessEngine(false, fileDirectory);
-            //engine.IdentifyDifferences();
-            //Value = {CV_8UC1}
-            engine.MatConvertTest();
+            var modeChoice = "q";
+            while (true)
+            {
+                Console.WriteLine("Start node in (s) server mode or (c) client mode?");
+                modeChoice = Console.ReadLine();
+                if(modeChoice == "s" || modeChoice == "c")
+                {
+                    break;
+                }
+            }
+
+            var fileDirectory = "";
+            if(modeChoice == "s")
+            {
+                while (true)
+                {
+                    Console.WriteLine("Please supply file directory");
+                    fileDirectory = Console.ReadLine();
+                    // Check that file directory is valid
+
+                    // If valid, break from loop
+                }
+            }
+            
+            var node = new Node(modeChoice == "s");
+            if (modeChoice == "s")
+            {
+                node.StartConnection(fileDirectory);
+            }
+            else
+            {
+                node.StartConnection();
+            }
         }
     }
 }
